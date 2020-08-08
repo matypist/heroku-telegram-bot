b44c929c56b51bb6925cf37f630c966511dd17ca
Matypist <69389461+matypist@users.noreply.github.com>
2020-08-08 14:38:23 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,10 @@
 import requests
 import redis
 
+#           Config vars
+token = os.environ['TELEGRAM_TOKEN']
+
 testoOriginale=Hook['params']['message']['text']
 idChat=Hook['params']['message']['chat']['id']
-URL='https://api.telegram.org/bot' + Hook['env']['token_ripeti'] + '/sendMessage'
+URL='https://api.telegram.org/bot' + Hook['env']['TELEGRAM_TOKEN'] + '/sendMessage'
 req=requests.get(URL,verify=False,data={'chat_id':idChat,'text':testoOriginale})
