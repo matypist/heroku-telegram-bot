1216eabc690ee6f129843be6944fbf120edc397d
Matypist <69389461+matypist@users.noreply.github.com>
2020-08-08 14:37:01 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,20 +1,8 @@
 # -*- coding: utf-8 -*-
+import requests
 import redis
-import os
-import telebot
-# import some_api_lib
-# import ...
 
-# Example of your code beginning
-#           Config vars
-token = os.environ['TELEGRAM_TOKEN']
-some_api_token = os.environ['SOME_API_TOKEN']
-#             ...
-
-# If you use redis, install this add-on https://elements.heroku.com/addons/heroku-redis
-r = redis.from_url(os.environ.get("REDIS_URL"))
-
-#       Your bot code below
-# bot = telebot.TeleBot(token)
-# some_api = some_api_lib.connect(some_api_token)
-#              ...
+testoOriginale=Hook['params']['message']['text']
+idChat=Hook['params']['message']['chat']['id']
+URL='https://api.telegram.org/bot' + Hook['env']['token_ripeti'] + '/sendMessage'
+req=requests.get(URL,verify=False,data={'chat_id':idChat,'text':testoOriginale})
